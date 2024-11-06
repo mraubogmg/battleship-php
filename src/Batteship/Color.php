2b582be6461129a1b9d809cd7b53603dc2a66493
trancended <trancended@gmail.com>
2024-11-06 10:06:27 +0100

    Update Color.php

--- src/Batteship/Color.php
+++ src/Batteship/Color.php
@@ -9,7 +9,7 @@ class Color
     const CADET_BLUE = "\e[34m";
     const RED = "\e[31m";
     const YELLOW = "\e[33m";
-    const ORANGE = "\e[33m";
+    const ORANGE = "\e[38;5;208m";
     const CHARTREUSE = "\e[32m";
     const PURPLE = "\e[35m";
 }
