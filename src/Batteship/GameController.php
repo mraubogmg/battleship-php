c3a7993a0551531a8a99e9ef0f9420e3dc59661e
trancended <trancended@gmail.com>
2024-11-07 11:17:15 +0100

    Update GameController.php

--- src/Batteship/GameController.php
+++ src/Batteship/GameController.php
@@ -20,6 +20,7 @@ class GameController
         foreach ($fleet as $ship) {
             foreach ($ship->getPositions() as $position) {
                 if ($position == $shot) {
+                    $ship->addShot($shot);
                     return true;
                 }
             }
