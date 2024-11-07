9583f086eaaa1bd7125840ee1a1d10fc57dd6a88
Mateusz Raubo <mateusz.raubo@morizon-gratka.pl>
2024-11-07 10:33:55 +0100

    taks-6 v2

--- src/ShipPresets.php
+++ src/ShipPresets.php
@@ -119,4 +119,156 @@ class ShipPresets
             ],
         ];
     }
+
+    public static function getPreset4()
+    {
+        return [
+            // Carrier (5 spaces) - Horizontal top
+            [
+                new Position('A', 1),
+                new Position('A', 2),
+                new Position('A', 3),
+                new Position('A', 4),
+                new Position('A', 5),
+            ],
+            // Battleship (4 spaces) - Vertical right
+            [
+                new Position('E', 8),
+                new Position('F', 8),
+                new Position('G', 8),
+                new Position('H', 8),
+            ],
+            // Cruiser (3 spaces) - Horizontal middle
+            [
+                new Position('D', 1),
+                new Position('D', 2),
+                new Position('D', 3),
+            ],
+            // Submarine (3 spaces) - Horizontal near bottom
+            [
+                new Position('G', 3),
+                new Position('G', 4),
+                new Position('G', 5),
+            ],
+            // Destroyer (2 spaces) - Vertical near center
+            [
+                new Position('C', 5),
+                new Position('D', 5),
+            ],
+        ];
+    }
+
+    public static function getPreset5()
+    {
+        return [
+            // Carrier (5 spaces) - Vertical left
+            [
+                new Position('A', 1),
+                new Position('B', 1),
+                new Position('C', 1),
+                new Position('D', 1),
+                new Position('E', 1),
+            ],
+            // Battleship (4 spaces) - Horizontal middle
+            [
+                new Position('D', 4),
+                new Position('D', 5),
+                new Position('D', 6),
+                new Position('D', 7),
+            ],
+            // Cruiser (3 spaces) - Vertical right
+            [
+                new Position('B', 8),
+                new Position('C', 8),
+                new Position('D', 8),
+            ],
+            // Submarine (3 spaces) - Horizontal bottom
+            [
+                new Position('H', 4),
+                new Position('H', 5),
+                new Position('H', 6),
+            ],
+            // Destroyer (2 spaces) - Vertical mid-left
+            [
+                new Position('F', 2),
+                new Position('G', 2),
+            ],
+        ];
+    }
+
+    public static function getPreset6()
+    {
+        return [
+            // Carrier (5 spaces) - Horizontal middle
+            [
+                new Position('D', 2),
+                new Position('D', 3),
+                new Position('D', 4),
+                new Position('D', 5),
+                new Position('D', 6),
+            ],
+            // Battleship (4 spaces) - Vertical left
+            [
+                new Position('A', 2),
+                new Position('B', 2),
+                new Position('C', 2),
+                new Position('D', 2),
+            ],
+            // Cruiser (3 spaces) - Horizontal top
+            [
+                new Position('B', 5),
+                new Position('B', 6),
+                new Position('B', 7),
+            ],
+            // Submarine (3 spaces) - Vertical right
+            [
+                new Position('F', 8),
+                new Position('G', 8),
+                new Position('H', 8),
+            ],
+            // Destroyer (2 spaces) - Horizontal bottom
+            [
+                new Position('G', 3),
+                new Position('G', 4),
+            ],
+        ];
+    }
+
+    public static function getPreset7()
+    {
+        return [
+            // Carrier (5 spaces) - Vertical center
+            [
+                new Position('B', 4),
+                new Position('C', 4),
+                new Position('D', 4),
+                new Position('E', 4),
+                new Position('F', 4),
+            ],
+            // Battleship (4 spaces) - Horizontal top
+            [
+                new Position('A', 1),
+                new Position('A', 2),
+                new Position('A', 3),
+                new Position('A', 4),
+            ],
+            // Cruiser (3 spaces) - Horizontal bottom
+            [
+                new Position('H', 5),
+                new Position('H', 6),
+                new Position('H', 7),
+            ],
+            // Submarine (3 spaces) - Vertical right
+            [
+                new Position('C', 7),
+                new Position('D', 7),
+                new Position('E', 7),
+            ],
+            // Destroyer (2 spaces) - Horizontal middle
+            [
+                new Position('F', 1),
+                new Position('F', 2),
+            ],
+        ];
+    }
 } 
